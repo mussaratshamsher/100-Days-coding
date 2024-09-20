765a23814f60a5f82555cbce36c7eb2b8529b2f6
mussaratshamsher <musaratskhan@github.com>
2024-09-20 05:20:18 -0700

    first

--- Day51-100/nextjs/nextjs-dashboard/app/page.tsx
+++ Day51-100/nextjs/nextjs-dashboard/app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-//import { Lusitana } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 import styles from '@/app/ui/home.module.css'
-//import { lusitana } from './ui/font';
+import { lusitana } from './ui/font';
 import Image from 'next/image';
 
 
@@ -14,7 +14,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
        <div className={styles.shape} > 
         
-        {/* <AcmeLogo /> */}
+         <AcmeLogo /> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
